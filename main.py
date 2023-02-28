7541473d2fc4da90ae27a73661062e8d043cc671
Nenisoft <nenisoft@Nenisoft-MacBook-Pro.local>
2023-02-28 22:09:19 +0100

    Implemented Try...Except and handled all exceptions

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 while True:
     user_action = input("Type Add, Show, Edit, Remove or Exit : ")
     user_action = user_action.strip()
-    if 'add' in user_action:
-        todo = user_action[4:] + '\n'
+    if user_action.startswith("add"):
+        todo = user_action[4:]
 
         with open("data/todos.txt", 'r') as file:
             todos = file.readlines()
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         with open("data/todos.txt", 'w') as file:
             file.writelines(todos)
 
-    elif 'show' in user_action:
+    elif user_action.startswith("show"):
         with open("data/todos.txt", 'r') as file:
             todos = file.readlines()
         for index, item in enumerate(todos):
             item = item.strip("\n")
             print(f"{index + 1}-{item.strip()}")
-    elif 'edit' in user_action:
-        number = int(input("Number of the ToDo to Edit: "))
-        number = number - 1
-        new_todo = input("Enter the New Todo: ")
-        with open("data/todos.txt", 'r') as file:
-            todos = file.readlines()
-        todos[number] = new_todo + "\n"
 
-        # updates the new todo
-        with open("data/todos.txt", "w") as file:
-            file.writelines(todos)
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+            new_todo = input("Enter the New Todo: ")
+            with open("data/todos.txt", 'r') as file:
+                todos = file.readlines()
+            todos[number] = new_todo + "\n"
 
-    elif 'remove' in user_action:
-        number = int(input("Number of ToDo to Remove: "))
-        with open("data/todos.txt", "r") as file:
-            todos = file.readlines()
-        todo_to_remove = todos[number - 1].strip("\n")
-        todos.pop(number - 1)
+            # updates the new todo
+            with open("data/todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Invalid Command")
+            continue
+        except IndexError:
+            print("There is no Todo Item with that Number")
+            continue
 
-        with open("data/todos.txt", "w") as file:
-            file.writelines(todos)
-        print(f"Todo: '{todo_to_remove}' has been removed")
+    elif user_action.startswith("remove"):
+        try:
+            number = int(input("Number of ToDo to Remove: "))
+            with open("data/todos.txt", "r") as file:
+                todos = file.readlines()
+            todo_to_remove = todos[number - 1].strip("\n")
+            todos.pop(number - 1)
 
-    elif 'exit' in user_action:
+            with open("data/todos.txt", "w") as file:
+                file.writelines(todos)
+            print(f"Todo: '{todo_to_remove}' has been removed")
+        except IndexError:
+            print("There is no Todo Item with that Number")
+            continue
+
+    elif user_action.startswith("exit"):
         break
     else:
         print("Invalid Command")
